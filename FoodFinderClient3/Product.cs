8c614b832e2fd49169a5ff5cf1f5fcb7db543cc4
AlinedaSilva <35935280+AlinedaSilva@users.noreply.github.com>
2018-08-28 08:54:14 +0100

    Adding Product Class

---
+++ FoodFinderClient3/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodFinderClient3
+{
+    class Product
+    {       
+            [JsonProperty("image")]
+            public string ImageURL { get; set; }
+            [JsonProperty("superDepartment")]
+            public string SuperDepartment { get; set; }
+            [JsonProperty("tpnb")]
+            public long TPNB { get; set; }
+            [JsonProperty("ContentsMeasureType")]
+            public string ContentsMeasureType { get; set; }
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("UnitOfSale")]
+            public int UnitOfSale { get; set; }
+            [JsonProperty("description")]
+            public IList<string> LstDescription { get; set; }
+            public string Description { get; set; }
+            [JsonProperty("AverageSellingUnitWeight")]
+            public decimal AverageSellingUnitWeight { get; set; }
+            [JsonProperty("UnitQuantity")]
+            public string UnitQuantity { get; set; }
+            [JsonProperty("id")]
+            public long ID { get; set; }
+            [JsonProperty("ContentsQuantity")]
+            public int ContentsQuantity { get; set; }
+            [JsonProperty("department")]
+            public string Department { get; set; }
+            [JsonProperty("price")]
+            public decimal Price { get; set; }
+            [JsonProperty("unitprice")]
+            public decimal UnitPrice { get; set; }
+        }
+    }
+
+
